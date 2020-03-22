610bf7a78373a3f1b6463e954f026c8595947e6e
root <291868060@qq.com>
2020-03-22 19:10:00 +0800

    3333

---
+++ src/LiuboDict.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * email 291868060@qq.com.
+ * User: Mr.liu
+ * Date: 2020/3/22
+ * Time: 19:08
+ */
+
+namespace LiuboDict;
+
+
+class LiuboDict
+{
+
+}
---
+++ src/LiuboDictTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * email 291868060@qq.com.
+ * User: Mr.liu
+ * Date: 2020/3/22
+ * Time: 19:09
+ */
+
+namespace LiuboDict;
+
+
+class LiuboDictTest
+{
+
+}
